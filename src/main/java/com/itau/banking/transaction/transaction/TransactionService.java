4afe66cb47dc3457c39ec563239314de0a12e84c
Eduardo Katsuki <eduardokatsuki4@gmail.com>
2025-11-29 19:10:38 -0400

    chore: removed unused package reference

--- src/main/java/com/itau/banking/transaction/transaction/TransactionService.java
+++ src/main/java/com/itau/banking/transaction/transaction/TransactionService.java
@@ -21,8 +21,6 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import java.math.BigDecimal;
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
